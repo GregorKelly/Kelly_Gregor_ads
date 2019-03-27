e756b9658c1eb7aa27c291de161ed6902f4ff31d
GregorKelly <gregorkelly@live.co.uk>
2019-03-27 14:41:03 +0000

    Final Submission 14:41 on 27th March 2019

---
+++ README.txt
@@ -0,0 +1,3 @@
+To compile, find the location of "coursework.c" usin command line
+use "cl courcework.c" to create the executable
+and run the executable once the program has created it
--- Source/coursework.c
+++ Source/coursework.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+//initialise the locations of the board
 char board[3][3] = {
 	//initializers
 	{'7','8','9'} ,
@@ -7,6 +7,7 @@ char board[3][3] = {
 	{'1','2','3'}
 };
 
+//prints the game board
 void printBoard() {
 	printf("      |      |      \n");
 	printf("   %c  |  %c   |  %c   \n", board[0][0], board[0][1], board[0][2]);
@@ -19,6 +20,7 @@ void printBoard() {
 	printf("      |      |      \n");
 }
 
+//creates pointers to indicate which boardspace is selected
 int indexToCoord(int index, int *row, int *col) {
 	if (index < 1 || index > 9) {
 		return 1; // 1 = wrong index
@@ -43,28 +45,32 @@ void playerTurn(char playerPointer) {
 	int row;
 	int col;
 	printf("\nPlayer %c, Enter a Number: ", playerPointer);
-
+	//check if  input is within the limits of the board
 	while (input < 1 || input > 9) {
 		input = getchar() - '0';
+		//checks if there is an error
 		if (input < 1) {
 			continue;
 		}
 		printf("\nInput = %d\n", input);
-
+		//checks for invalid input
 		if (indexToCoord(input, &row, &col)) {
+			input = -1;
 			printf("Location invalid, please select again: ");
 		}
-
+		//checks if space has already been typed in
 		if (checkBox(row, col)) {
 			input = -1;
 			printf("Space already occupied, please select again: ");
 		}
 	}
+	//set the game to the pointer
 	board[row][col] = playerPointer;
 }
 
 int checkBox(int row, int col) {
 	int t = 0;
+	//check for player piece
 	if (board[row][col] == 'X' || board[row][col] == 'O') {
 		t = 1;
 	}
@@ -81,10 +87,12 @@ int checkWin(char playerPointer) {
 	for (int i = 0; i < 8; i++) {
 		counter = 0;
 		for (int j = 0; j < 3; j++) {
+			//check for incorrect index
 			if (indexToCoord(winConditions[i][j], &row, &col) == 1) {
-				printf("No such index exists\n");
+				printf("No such index exists\n"); 
 			}
 			else {
+				//check win conditions
 				if (board[row][col] == playerPointer) {
 					counter = counter + 1;
 					if (counter == 3) {
@@ -110,12 +118,14 @@ int main(int argc, char **argv) {
 	char playerPointer = 'O';
 
 	for (int moves = 0; moves < 9; moves++) {
+		//change player
 		if (playerPointer == 'X') {
 			playerPointer = 'O';
 		}
 		else {
 			playerPointer = 'X';
 		}
+		//play turn
 		playerTurn(playerPointer);
 		printBoard();
 		if (checkWin(playerPointer) == 1) {
